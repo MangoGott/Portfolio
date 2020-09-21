b4db3c5d2421638c33c66fbf9a5994ec31f415e6
Robin Kammerlander <robinkammerlander@Robins-MacBook-Pro.local>
2020-09-21 15:19:58 +0200

    moved to Contentful CMS

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,9 @@
 const path = require('path')
 
+/* 
+### För lokala .md
+###
+###
 module.exports.onCreateNode = ({ node, actions }) => {
     const { createNodeField } = actions
 
@@ -13,30 +17,30 @@ module.exports.onCreateNode = ({ node, actions }) => {
         })
     }
 }
+*/
+
 
 module.exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions
     const blogTemplate = path.resolve('./src/templates/blog.js')
     const res = await graphql(`
     query {
-        allMarkdownRemark {
+        allContentfulBlogPost {
             edges {
                 node {
-                    fields {
-                        slug
-                    }
+                    slug
                 }
             }
         }
     }
     `)
 
-    res.data.allMarkdownRemark.edges.forEach((edge) => {
+    res.data.allContentfulBlogPost.edges.forEach((edge) => {
         createPage({
             component: blogTemplate,
-            path: `/blog/${edge.node.fields.slug}`,
+            path: `/blog/${edge.node.slug}`,
             context: {
-                slug: edge.node.fields.slug
+                slug: edge.node.slug
             }
         })
     })
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,6 +7,53 @@ import blogStyles from './blog.module.scss'
 const BlogPage = () => {
     const data = useStaticQuery(graphql`
     query {
+        allContentfulBlogPost ( sort: { fields: publishedDate, order: DESC } ) {
+            edges {
+                node {
+                    title
+                    slug
+                    publishedDate(formatString: "MMMM Do, YYYY")
+                }
+            }
+        }
+    }
+    `)
+    //console.log("HEJ MANNEN")
+    //console.log(data.allMarkdownRemark.edges[0].node.frontmatter.title)
+    return (
+        <Layout>
+            <h1>Blog</h1>
+            <ol className={blogStyles.posts}>
+                {data.allContentfulBlogPost.edges.map((edge) => {
+                    return (
+                        <li className={blogStyles.post}>
+                            <Link to={`/blog/${edge.node.slug}`}>
+                            <h2>{edge.node.title}</h2>
+                            <p>{edge.node.publishedDate}</p>
+                            </Link>
+                        </li>
+                    )
+                })}
+            </ol>
+        </Layout>
+    )
+}
+
+export default BlogPage
+
+/*
+### Om du vill använda lokala .md filer (markdown).
+###
+###
+import React from 'react'
+import {graphql, Link, useStaticQuery} from 'gatsby'
+
+import Layout from '../components/layout'
+import blogStyles from './blog.module.scss'
+
+const BlogPage = () => {
+    const data = useStaticQuery(graphql`
+    query {
         allMarkdownRemark {
             edges {
                 node {
@@ -44,3 +91,6 @@ const BlogPage = () => {
 }
 
 export default BlogPage
+
+
+*/
